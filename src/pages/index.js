47e6428650b86d1b621c065f67d641b65481daa6
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2019-11-19 14:12:41 +0000

    links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ color: `purple` }}>
+    <Link to="/contact/">Contact</Link>
+    <Header headerText="Hello Gatsby!" />
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <div style={{height: "200px", backgroundColor: "purple", width: "400px"}}>Bum</div>
+    <a href="https://ull-mii-sytws-1920.github.io/gatsby-hello-world/">GitHub pages</a>
+  </div>
+)
